4e7142204f25f1e21af68f35bd84721686e1f9ee
Tatsuya Murakami <msgtatsuya@gmail.com>
2026-03-11 16:27:21 +0900

    Add oxlint

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    const label : string = "mcount";
+
+    return (
+        <div>
+        <button onClick={() => setCount((c) => c + 1)}>
+            {label} is {count}
+        </button>
+        </div>
+    )
+}
+
+export default App
