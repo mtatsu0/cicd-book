88f30fe9145ab8f2875599bec8219ebc96fc8aa7
Tatsuya Murakami <msgtatsuya@gmail.com>
2026-03-12 23:56:27 +0900

    Add oxfmt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    const label : string = "mcount";
+  const label: string = "mcount";
 
-    return (
-        <div>
-        <button onClick={() => { setCount((c) => c + 1) }}>
-            {label} is {count}
-        </button>
-        </div>
-    )
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount((c) => c + 1);
+        }}
+      >
+        {label} is {count}
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
